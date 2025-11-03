10b28d93ce2a8f2cf9d9a9797049adc562d4eaf6
andrewf1amex <flamexnivelson@gmail.com>
2025-11-03 14:20:30 +0700

    mutex + rw implementation added (not finished yet)

---
+++ 2.3/rw/list.c
@@ -0,0 +1,75 @@
+#define _GNU_SOURCE
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+Storage *storage_init(int n) 
+{
+    if (n <= 0) return NULL;
+
+    Storage *s = malloc(sizeof(Storage));
+    if (!s) 
+    {
+        perror("malloc storage");
+        exit(1);
+    }
+
+    s->first = NULL;
+    s->count = n;
+
+    srand(time(NULL));
+
+    Node *prev = NULL;
+    for (int i = 0; i < n; i++) 
+    {
+        Node *node = malloc(sizeof(Node));
+        if (!node) 
+        {
+            perror("malloc node");
+            exit(1);
+        }
+
+        int len = rand() % 100 + 1;
+        for (int j = 0; j < len; j++) node->value[j] = 'a' + rand() % 26;
+        node->value[len] = '\0';
+
+        pthread_rwlock_init(&node->sync, NULL);
+        node->next = NULL;
+
+        if (prev == NULL) s->first = node;
+        else prev->next = node;
+
+        prev = node;
+    }
+
+    return s;
+}
+
+void storage_destroy(Storage *s) 
+{
+    if (!s) return;
+    Node *cur = s->first;
+    while (cur) 
+    {
+        Node *next = cur->next;
+        pthread_rwlock_destroy(&cur->sync);
+        free(cur);
+        cur = next;
+    }
+
+    free(s);
+}
+
+void storage_print(Storage *s) 
+{
+    if (!s) return;
+    Node *cur = s->first;
+    int i = 0;
+    while (cur) 
+    {
+        printf("[%03d] \"%s\"\n", i++, cur->value);
+        cur = cur->next;
+    }
+}
---
+++ 2.3/rw/list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <pthread.h>
+
+typedef struct _Node{
+    char value[100];
+    struct _Node *next;
+    pthread_rwlock_t sync;
+}
+
+typedef struct _Storage{
+    Node *first;
+    int count;
+} Storage;
+
+Storage *storage_init(int n);
+void storage_destroy(Storage *s);
+void storage_print(Storage *s);
+
+#endif
