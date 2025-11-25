903fce02397ddb7d49603e7a0012ac608e6ad1e3
andrewf1amex <flamexnivelson@gmail.com>
2025-11-25 14:03:39 +0700

    mutex full task requirements implementation added

--- 2.3/mutex/main_mutex.c
+++ 2.3/mutex/main_mutex.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void *thread_asc(void *);
 void *thread_desc(void *);
@@ -14,10 +15,24 @@ void *thread_monitor(void *);
 
 int main(int argc, char **argv)
 {
-    Storage *s = storage_init(1000);
+    int n = 1000;
+
+    if (argc > 1) 
+    {
+        n = atoi(argv[1]);
+        if (n <= 0) 
+        {
+            fprintf(stderr, "Usage: %s [list_size > 0]\n", argv[0]);
+            return 1;
+        }
+    }
+
+    srand((unsigned int)time(NULL));
+
+    Storage *s = storage_init(n);
     if (!s) 
     {
-        fprintf(stderr, "storage_init failed\n");
+        fprintf(stderr, "storage_init(%d) failed\n", n);
         return 1;
     }
 
@@ -25,21 +40,16 @@ int main(int argc, char **argv)
     pthread_t t_sw_asc, t_sw_desc, t_sw_equal;
     pthread_t t_monitor;
 
-    pthread_create(&t_asc,      NULL, thread_asc,       s);
-    pthread_create(&t_desc,     NULL, thread_desc,      s);
-    pthread_create(&t_equal,    NULL, thread_equal,     s);
-    pthread_create(&t_sw_asc,   NULL, thread_swap_asc,  s);
-    pthread_create(&t_sw_desc,  NULL, thread_swap_desc, s);
-    pthread_create(&t_sw_equal, NULL, thread_swap_equal,s);
-    pthread_create(&t_monitor,  NULL, thread_monitor,   NULL);
+    pthread_create(&t_asc, NULL, thread_asc, s);
+    pthread_create(&t_desc, NULL, thread_desc, s);
+    pthread_create(&t_equal, NULL, thread_equal, s);
 
-    pthread_join(t_asc,      NULL);
-    pthread_join(t_desc,     NULL);
-    pthread_join(t_equal,    NULL);
-    pthread_join(t_sw_asc,   NULL);
-    pthread_join(t_sw_desc,  NULL);
-    pthread_join(t_sw_equal, NULL);
-    pthread_join(t_monitor,  NULL);
+    pthread_create(&t_sw_asc, NULL, thread_swap_asc, s);
+    pthread_create(&t_sw_desc, NULL, thread_swap_desc, s);
+    pthread_create(&t_sw_equal, NULL, thread_swap_equal, s);
+
+    pthread_create(&t_monitor, NULL, thread_monitor, NULL);
+    pthread_join(t_monitor, NULL);
 
     storage_destroy(s);
     return 0;
--- 2.3/mutex/threads.c
+++ 2.3/mutex/threads.c
@@ -9,12 +9,17 @@
 #include <time.h>
 #include <unistd.h>
 
-static long asc_iterations   = 0;
-static long desc_iterations  = 0;
+static long asc_iterations = 0;
+static long desc_iterations = 0;
 static long equal_iterations = 0;
-static long asc_swaps   = 0;
-static long desc_swaps  = 0;
-static long equal_swaps = 0;
+
+static long asc_pairs = 0; // пары с l1 <  l2
+static long desc_pairs = 0; // пары с l1 >  l2
+static long equal_pairs = 0; // пары с l1 == l2
+
+static long asc_swaps = 0; // свопы, исправляющие "не-возрастание"
+static long desc_swaps = 0; // свопы, исправляющие "не-убывание"
+static long equal_swaps = 0; // свопы, исправляющие "не-равенство"
 
 static pthread_mutex_t counters_lock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -39,11 +44,12 @@ static void snapshot_counters(long *ai, long *di, long *ei, long *as, long *ds, 
 
 typedef int (*pair_predicate)(int l1, int l2);
 
-static int pred_asc(int l1, int l2)   { return l1 < l2; }
-static int pred_desc(int l1, int l2)  { return l1 > l2; }
+static int pred_less(int l1, int l2) { return l1 <  l2; }
+static int pred_greater(int l1, int l2) { return l1 >  l2; }
 static int pred_equal(int l1, int l2) { return l1 == l2; }
+static int pred_not_equal(int l1, int l2) { return l1 !=  l2; }
 
-static void traverse_list(Storage *s, pair_predicate pred, long *iter_counter)
+static void traverse_list(Storage *s, pair_predicate match_pred,long *iter_counter, long *pairs_counter)
 {
     while (1)
     {
@@ -73,6 +79,8 @@ static void traverse_list(Storage *s, pair_predicate pred, long *iter_counter)
             int l1 = (int)strlen(a->value);
             int l2 = (int)strlen(b->value);
 
+            if (match_pred(l1, l2)) inc_long(pairs_counter);
+
             pthread_mutex_unlock(&a->sync);
             a = b;
         }
@@ -84,19 +92,19 @@ static void traverse_list(Storage *s, pair_predicate pred, long *iter_counter)
 
 void *thread_asc(void *arg)
 {
-    traverse_list((Storage *)arg, pred_asc, &asc_iterations);
+    traverse_list((Storage *)arg, pred_less, &asc_iterations, &asc_pairs);
     return NULL;
 }
 
 void *thread_desc(void *arg)
 {
-    traverse_list((Storage *)arg, pred_desc, &desc_iterations);
+    traverse_list((Storage *)arg, pred_greater, &desc_iterations, &desc_pairs);
     return NULL;
 }
 
 void *thread_equal(void *arg)
 {
-    traverse_list((Storage *)arg, pred_equal, &equal_iterations);
+    traverse_list((Storage *)arg, pred_equal, &equal_iterations, &equal_pairs);
     return NULL;
 }
 
@@ -104,7 +112,7 @@ static void do_random_swap(Storage *s, pair_predicate need_swap, long *swap_coun
 {
     if (s->count < 2) return;
 
-    int index = rand_r(seed) % (s->count - 1); 
+    int index = rand_r(seed) % (s->count - 1);
 
     Node *prev = s->first;
     pthread_mutex_lock(&prev->sync);
@@ -161,7 +169,7 @@ void *thread_swap_asc(void *arg)
 
     while (1)
     {
-        do_random_swap(s, pred_asc, &asc_swaps, &seed);
+        do_random_swap(s, pred_greater, &asc_swaps, &seed);
         sched_yield();
     }
     return NULL;
@@ -174,7 +182,7 @@ void *thread_swap_desc(void *arg)
 
     while (1)
     {
-        do_random_swap(s, pred_desc, &desc_swaps, &seed);
+        do_random_swap(s, pred_less, &desc_swaps, &seed);
         sched_yield();
     }
     return NULL;
@@ -187,7 +195,7 @@ void *thread_swap_equal(void *arg)
 
     while (1)
     {
-        do_random_swap(s, pred_equal, &equal_swaps, &seed);
+        do_random_swap(s, pred_not_equal, &equal_swaps, &seed);
         sched_yield();
     }
     return NULL;
